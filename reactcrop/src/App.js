7b6614807fe424ef3a92d396775914ca2da80746
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2023-06-28 12:43:02 +0530

    ..

--- reactcrop/src/App.js
+++ reactcrop/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import ImageCropDialog from "./ImageCropDialog";
+
+
+const initData = [
+  {
+    id: 1,
+    imageUrl: "q1.jpg",
+    croppedImageUrl: null,
+  },
+  // {
+  //   id: 2,
+  //   imageUrl: "q2.jpg",
+  //   croppedImageUrl: null,
+  // },
+  
+];
 
 function App() {
+  const [images, setImages] = useState(initData);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const onCancel = () => {
+    setSelectedImage(null);
+  };
+
+  const setCroppedImageFor = (id, crop, zoom, aspect, croppedImageUrl) => {
+    const newImagesList = [...images];
+    const imageIndex = images.findIndex((x) => x.id === id);
+    const image = images[imageIndex];
+    const newImage = { ...image, croppedImageUrl, crop, zoom, aspect };
+    newImagesList[imageIndex] = newImage;
+    setImages(newImagesList);
+    setSelectedImage(null);
+  };
+
+  const resetImage = (id) => {
+    setCroppedImageFor(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedImage ? (
+        <ImageCropDialog
+          id={setSelectedImage.id}
+          imageUrl={selectedImage.imageUrl}
+          cropInit={selectedImage.crop}
+          zoomInit={selectedImage.zoom}
+          aspectInit={selectedImage.aspect}
+          onCancel={onCancel}
+          setCroppedImageFor={setCroppedImageFor}
+          resetImage={resetImage}
+        />
+      ) : null}
+      {images.map((image) => (
+        <div className="imageCard" key={image.id}>
+          <img
+            src={image.croppedImageUrl ? image.croppedImageUrl : image.imageUrl}
+            onClick={() => {
+              console.log(image);
+              setSelectedImage(image);
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
